cb94219aeb72ad435156b5e88bc763760ba68c66
João Saraiva <saraiva@di.uminho.pt>
2026-06-17 11:00:02 +0100

    Change so that shrinking of a string doesn't produce the empty string

--- Examples/Let/LetGenerator.hs
+++ Examples/Let/LetGenerator.hs
@@ -286,10 +286,10 @@ shrinkLet (Let l e) = [ Let l' e' | l' <- shrinkList l , e' <- shrinkExp e ]
 shrinkList :: List -> [List]
 shrinkList EmptyList = [] 
 shrinkList (NestedLet n l lst) = [ NestedLet n' l' EmptyList
-                                 | n' <- shrink n , l' <- shrinkLet l ] 
+                                 | n' <- shrink n , l' <- shrinkLet l , not (null n')  ] 
                                  ++ shrinkList lst
-shrinkList (Assign    n e lst) = [ Assign n e EmptyList
-                                 | n' <- shrink n , e' <- shrinkExp e ] 
+shrinkList (Assign    n e lst) = [ Assign n' e' EmptyList
+                                 | n' <- shrink n , e' <- shrinkExp e , not (null n')  ] 
                                  ++ shrinkList lst
 
 shrinkExp :: Exp -> [Exp]
@@ -301,5 +301,6 @@ shrinkExp (Sub e1 e2)   = [e1, e2] ++
                           [Sub e1 e2'  | e2' <- shrinkExp e2]
 shrinkExp (Neg e)       = [e] ++
                           [Neg e'      | e'  <- shrinkExp e] 
-shrinkExp (Var n)       = [Var n'      | n'  <- shrink n ]
+shrinkExp (Var n)       = [Var n'      | n'  <- shrink n
+                                       , not (null n')     ]
 shrinkExp (Const c)     = [Const c'    | c'  <- shrink c ]
